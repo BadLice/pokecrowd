61a0e60aa4e6acc0e14d7fa1f4453223b2e024a3
alicata <alex.licata@grupposandonato.com>
2024-08-31 19:02:19 +0200

    improve divider component with direction and custom style

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,7 +1,24 @@
 import { StyleSheet, View } from "react-native";
+import { StyleProps } from "react-native-reanimated";
 
-export const Divider = () => <View style={styles.divider}></View>;
+export const Divider = ({
+  direction = "horizontal",
+  style,
+}: {
+  direction?: "horizontal" | "vertical";
+  style?: StyleProps;
+}) => (
+  <View
+    style={[
+      direction === "horizontal"
+        ? styles.dividerHorizontal
+        : styles.dividerVertical,
+      style,
+    ]}
+  ></View>
+);
 
 const styles = StyleSheet.create({
-  divider: { width: 1, backgroundColor: "black" },
+  dividerHorizontal: { height: 1, backgroundColor: "grey" },
+  dividerVertical: { width: 1, backgroundColor: "grey" },
 });
