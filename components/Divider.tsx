88b9d13bf603dabc3f46638bae12035e8fbf6053
alicata <alex.licata@grupposandonato.com>
2024-08-31 17:24:24 +0200

    create divider component

---
+++ components/Divider.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet, View } from "react-native";
+
+export const Divider = () => <View style={styles.divider}></View>;
+
+const styles = StyleSheet.create({
+  divider: { width: 1, backgroundColor: "black" },
+});
