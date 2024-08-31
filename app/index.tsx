8f7ee1845c06cea4ff4758c04729aef3309e81fa
alicata <alex.licata@grupposandonato.com>
2024-08-31 19:01:46 +0200

    rework routing: - remove tabs - use only 2 routes without nesting

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,24 @@
-import { Image, StyleSheet } from "react-native";
+import { Image, SafeAreaView, StyleSheet } from "react-native";
 import { ThemedView } from "@/components/ThemedView";
 import { PokemonList } from "@/components/PokemonList";
 
 export default function HomeScreen() {
   return (
-    <>
+    <SafeAreaView>
       <ThemedView
         style={{ height: "20%" }}
         lightColor={"#A1CEDC"}
         darkColor={"#1D3D47"}
       >
         <Image
-          source={require("@/assets/images/partial-react-logo.png")}
+          source={{
+            uri: "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png",
+          }}
           style={styles.reactLogo}
         />
       </ThemedView>
       <PokemonList />
-    </>
+    </SafeAreaView>
   );
 }
 
@@ -26,8 +28,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 170,
-    width: 290,
+    height: "100%",
+    width: "100%",
     top: 0,
     left: 0,
     position: "absolute",
